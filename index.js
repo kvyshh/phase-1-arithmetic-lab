765531b0f2a12f03bf955bc8516ca449992d59c5
Kindred Lee <kindred4lee@gmail.com>
2021-10-14 22:01:46 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let num1 = 2;
-let num2 = 31;
-
+let num1 = 31;
+let num2 = 2;
 const multiply = num1 * num2;
 
-const random = Math.floor(Math.random() * 10000) +1;
-let num3 = 10;
-let num4 = 6;
-const mod = num3 % num4;
-const max = Math.max(20,1);
+const random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 16;
+let num4 = 4;
+const mod = num4 % num3;
+
+const max = Math.max(20);
